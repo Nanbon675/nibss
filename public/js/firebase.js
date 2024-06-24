9ccfdde586d0f89a0d10a834d59defb0c38864de
Nanbon675 <167584843+Nanbon675@users.noreply.github.com>
2024-06-25 00:58:19 +0300

    Add files via upload

---
+++ public/js/firebase.js
@@ -0,0 +1,12 @@
+let firebaseConfig = {
+  apiKey: "AIzaSyDTRZtlq7r-514R7U7tKJwHKZe3cep7GXM",
+  authDomain: "shareinfo-eca64.firebaseapp.com",
+  projectId: "shareinfo-eca64",
+  storageBucket: "shareinfo-eca64.appspot.com",
+  messagingSenderId: "90294740912",
+  appId: "1:90294740912:web:b473e7f3df140916a61875"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
